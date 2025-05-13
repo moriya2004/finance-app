f01de1875de9465930f8e1286e74aee8191b523b
moriya meshulami <mor012004@gmail.com>
2025-05-13 21:28:41 +0300

    Recharts and Key Performance Indicators UI

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,13 +1,12 @@
-console.log(import.meta.env.VITE_BASE_URL);
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./types";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
   tagTypes: ["Kpis"],
   endpoints: (build) => ({
-    getKpis: build.query<void, void>({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
       query: () => "kpi/kpis/",
       providesTags: ["Kpis"],
     }),
